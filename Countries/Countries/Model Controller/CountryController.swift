4cd0c08119d2f705bd75c723181bac6c94bd762f
Nelson Gonzalez <nelglezfl@gmail.com>
2019-04-09 16:51:31 -0400

    Add Model Controller with fetching URLSession request

--- Countries/Countries/Model Controller/CountryController.swift
+++ Countries/Countries/Model Controller/CountryController.swift
@@ -10,5 +10,38 @@ import Foundation
 
 class CountryController {
     
+    private(set) var countries: [Country] = []
+    private let baseUrl = URL(string: "https://restcountries.eu/rest/v2/name")!
+    
+    
+    func fetchCountries(searchTerm: String, completion: @escaping(Error?) -> Void) {
+        let url = baseUrl.appendingPathComponent(searchTerm)
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching the countries: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data")
+                completion(error)
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let countries = try decoder.decode([Country].self, from: data)
+                self.countries = countries
+            } catch {
+                NSLog("Error decoding: \(error)")
+                completion(error)
+                return
+            }
+            
+        }.resume()
+    }
     
 }
