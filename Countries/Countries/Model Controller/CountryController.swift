7db90048425841b9e6e232b050968d3419fb5d3d
Nelson Gonzalez <nelglezfl@gmail.com>
2019-04-09 16:23:46 -0400

    Add Xcode and files to project

---
+++ Countries/Countries/Model Controller/CountryController.swift
@@ -0,0 +1,14 @@
+//
+//  CountryController.swift
+//  Countries
+//
+//  Created by Nelson Gonzalez on 4/9/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import Foundation
+
+class CountryController {
+    
+    
+}
---
+++ Countries/Countries/Model/Country.swift
@@ -0,0 +1,9 @@
+//
+//  Country.swift
+//  Countries
+//
+//  Created by Nelson Gonzalez on 4/9/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import Foundation
